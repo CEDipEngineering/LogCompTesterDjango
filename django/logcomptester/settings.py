6ca4e6c696625b5253669738ac879e44efbc7edd
Ubuntu <ubuntu@ip-172-31-92-246.ec2.internal>
2022-10-16 10:02:06 +0000

    Hide and update django admin secret key

--- django/logcomptester/settings.py
+++ django/logcomptester/settings.py
@@ -21,8 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+# Read config file manually, because django hates me
+SECRET_FILE_PATH = str(BASE_DIR.parent / 'env' / 'django_cred.cnf')
+parser = configparser.ConfigParser()
+parser.read(SECRET_FILE_PATH)
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tnjccjscrwmspqveep!ntu+$9u&4)b22=%(zt5r-&o4*m_gwi_'
+SECRET_KEY = parser['admin']['password']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
